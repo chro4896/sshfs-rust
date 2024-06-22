24b2fc7123d3e18329ed940c7e0552a0a4eb7251
chro4896 <11955521+chro4896@users.noreply.github.com>
2024-06-22 11:48:59 +0900

    create prototype of sshfs_unlink

--- sshfs-rust/src/lib.rs
+++ sshfs-rust/src/lib.rs
@@ -12,3 +12,11 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
+
+#[no_mangle]
+pub extern "C" fn sshfs_unlink(path: *const core::ffi::c_char) -> core::ffi::c_int {
+	let path = unsafe { CStr::from_ptr(path) };
+	let path = path.to_str().unwrap();
+	println!("unlink: {}", path);
+	0 as c_int
+}
