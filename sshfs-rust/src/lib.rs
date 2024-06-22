d72f14c52cd215abd2d5ec272886ec8cc3d8f281
chro4896 <11955521+chro4896@users.noreply.github.com>
2024-06-22 12:15:20 +0900

    create prototype sftp_request in C

--- sshfs-rust/src/lib.rs
+++ sshfs-rust/src/lib.rs
@@ -13,6 +13,18 @@ mod tests {
     }
 }
 
+#[repr(C)]
+struct Buffer {
+	p: *const u8,
+	len: usize,
+	size: usize
+}
+
+extern "C" {
+	fn get_conn(sshfs_file: *const core::ffi::c_void, path: *const core::ffi::c_void) -> *mut core::ffi::c_void;
+	fn sftp_request(conn: *mut core::ffi::c_void, type: u8, buf: *const Buffer, expect_type: u8, outbuf: *mut Buffer) -> core::ffi::c_int;
+}
+
 #[no_mangle]
 pub extern "C" fn sshfs_unlink(path: *const core::ffi::c_char) -> core::ffi::c_int {
 	let path = unsafe { core::ffi::CStr::from_ptr(path) };
