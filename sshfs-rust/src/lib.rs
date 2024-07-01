b688143af7f42354820ee54f6fae78e395bf81f4
chro4896 <11955521+chro4896@users.noreply.github.com>
2024-07-01 22:54:19 +0900

    create prototype of sshfs_link

--- sshfs-rust/src/lib.rs
+++ sshfs-rust/src/lib.rs
@@ -12,3 +12,12 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
+
+#[no_mangle]
+pub extern "C" fn sshfs_link(from_path: *const core::ffi::c_char, to_path: *const core::ffi::c_char) -> core::ffi::c_int {
+	let from_path = unsafe { core::ffi::CStr::from_ptr(from_path) };
+	let from_path = from_path.to_bytes();
+	let to_path = unsafe { core::ffi::CStr::from_ptr(to_path) };
+	let to_path = to_path.to_bytes();
+	0 as core::ffi::c_int
+}
