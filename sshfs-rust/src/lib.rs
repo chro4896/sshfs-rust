6637ccf15ba2aa817956452e82d9e2f456645e35
chro4896 <11955521+chro4896@users.noreply.github.com>
2024-07-02 07:15:35 +0900

    rename Buffer into Buffer_sys and create struct Buffer for rust

--- sshfs-rust/src/lib.rs
+++ sshfs-rust/src/lib.rs
@@ -6,15 +6,30 @@ extern "C" {
 }
 
 #[repr(C)]
-struct Buffer {
+struct Buffer_sys {
 	p: *const u8,
 	len: usize,
 	size: usize
 }
 
+struct Buffer {
+	p: Vec<u8>,
+	len: usize
+}
+
+impl Buffer {
+	fn translate_into_sys (&self) -> Buffer_sys {
+		{
+			p: unsafe { self.p.as_ptr() },
+			len: self.len,
+			size: self.p.len
+		}
+	}
+}
+
 extern "C" {
 	fn get_conn(sshfs_file: *const core::ffi::c_void, path: *const core::ffi::c_void) -> *mut core::ffi::c_void;
-	fn sftp_request(conn: *mut core::ffi::c_void, ssh_op_type: u8, buf: *const Buffer, expect_type: u8, outbuf: *mut Buffer) -> core::ffi::c_int;
+	fn sftp_request(conn: *mut core::ffi::c_void, ssh_op_type: u8, buf: *const Buffer_sys, expect_type: u8, outbuf: *mut Buffer_sys) -> core::ffi::c_int;
 }
 
 #[no_mangle]
