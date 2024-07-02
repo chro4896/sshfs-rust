1e44f5f145b12bfa8dbfb875d1ff0768f2d8b8bb
chro4896 <11955521+chro4896@users.noreply.github.com>
2024-07-02 19:24:27 +0900

    modify random_string

--- sshfs-rust/src/lib.rs
+++ sshfs-rust/src/lib.rs
@@ -4,10 +4,10 @@ use rand::Rng;
 pub extern "C" fn random_string(s_ptr: *mut core::ffi::c_char, length: core::ffi::c_int) {
 	for idx in 0..length {
 		unsafe {
-			*s_ptr.offset(idx) = b'0' + rand::thread_rng().gen_range(0..10);
+			*s_ptr.offset(idx.try_into().unwrap()) = (b'0' as c_char) + rand::thread_rng().gen_range(0..10);
 		}
 	}
 	unsafe {
-		*s_ptr.offset(length) = 0;
+		*s_ptr.offset(length.try_into().unwrap()) = 0;
 	}
 }
