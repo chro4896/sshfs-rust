c1edcfbbdd7cfd759128c305b9a46e409cf1d5b8
chro4896 <11955521+chro4896@users.noreply.github.com>
2024-07-02 19:12:16 +0900

    convert random_string into rust

--- sshfs-rust/src/lib.rs
+++ sshfs-rust/src/lib.rs
@@ -1,3 +1,13 @@
+use rand::Rng;
+
 #[no_mangle]
 pub extern "C" fn random_string(s_ptr: *mut core::ffi::c_char, length: core::ffi::c_int) {
+	for idx in 0..length {
+		unsafe {
+			*s_ptr.offset(idx) = b'0' + rand::thread_rng().gen_range(0..10);
+		}
+	}
+	unsafe {
+		*s_ptr.offset(length) = 0;
+	}
 }
