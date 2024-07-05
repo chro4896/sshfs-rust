fcb3eae58b2d633eed826d8884ff0a0f00e3d4e9
chro4896 <11955521+chro4896@users.noreply.github.com>
2024-07-05 23:04:01 +0900

    modify prototype of sftp_request_wait_rust

--- sshfs-rust/src/lib.rs
+++ sshfs-rust/src/lib.rs
@@ -39,6 +39,8 @@ struct List_head {
 pub extern "C" fn sftp_request_wait_rust(req: *mut Request_ext_rust, op_type: u8, expect_type: u8, outbuf: *mut core::ffi::c_void, req_orig: *mut core::ffi::c_void) -> core::ffi::c_int {
 	let mut err = 0;
 	
+	let mut req = unsafe { req.as_mut().unwrap() };
+	
 	if req.error != 0 {
 		err = req.error;
 	} else {
