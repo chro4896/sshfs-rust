eb442e202efe130535ed8c767b8223046890dfab
chro4896 <11955521+chro4896@users.noreply.github.com>
2024-07-05 22:56:19 +0900

    create Request_ext_rust and prototype of sftp_request_wait_rust

--- sshfs-rust/src/lib.rs
+++ sshfs-rust/src/lib.rs
@@ -12,3 +12,45 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
+
+#[repr(C)]
+struct Request_ext_rust {
+	want_reply: core::ffi::c_uint,
+	ready: *mut libc::sem_t,
+	reply_type: u8,
+	id: u32,
+	replied: core::ffi::c_int,
+	error: core::ffi::c_int,
+	reply: *mut core::ffi::c_void,
+	start: libc::timeval,
+	data: *mut core::ffi::c_void,
+	end_func: *mut core::ffi::c_void,
+	len: core::ffi::c_size_t,
+	list: List_head,
+	conn: *mut core::ffi::c_void,
+}
+
+#[repr(C)]
+struct List_head {
+	prev: *mut List_head,
+	next: *mut List_head,
+}
+
+pub extern "C" fn sftp_request_wait_rust(req: *mut Reuest_ext_rust, op_type: u8, expect_type: u8, outbuf: *mut core::ffi::c_void, req_orig: *mut core::ffi::c_void) -> core::ffi::c_int {
+	let mut err = 0;
+	
+	if req.error != 0 {
+		err = req.error;
+	} else {
+		loop {
+			if libc::sem_wait(req.ready) != 0 {
+				break;
+			}
+		}
+		if req.error != 0 {
+			err = req.error;
+		} else {
+			
+		}
+	}
+}
