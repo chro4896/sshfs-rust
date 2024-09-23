f1a9229f5a84b8f30e6485f645c610b311185469
chro4896 <11955521+chro4896@users.noreply.github.com>
2024-09-24 06:58:32 +0900

    add constant SSH_FXP_NAME

--- sshfs-rust/src/lib.rs
+++ sshfs-rust/src/lib.rs
@@ -10,6 +10,7 @@ const SSH_FXP_RMDIR: u8 = 15;
 const SSH_FXP_RENAME: u8 = 18;
 const SSH_FXP_STATUS: u8 = 101;
 const SSH_FXP_HANDLE: u8 = 102;
+const SSH_FXP_NAME: u8 = 104;
 const SSH_FXP_EXTENDED: u8 = 200;
 
 const SSH_FILEXFER_ATTR_PERMISSIONS: u32 = 4;
