efb144b3994f833739d21955304666de84301f6b
chro4896 <11955521+chro4896@users.noreply.github.com>
2024-09-24 07:22:14 +0900

    run rustfmt

--- sshfs-rust/src/lib.rs
+++ sshfs-rust/src/lib.rs
@@ -446,13 +446,15 @@ pub unsafe extern "C" fn request_free(req: *mut Request) {
 
 #[no_mangle]
 pub extern "C" fn malloc_conn() -> *mut Conn {
-	let conn_ptr = unsafe { libc::calloc(1, std::mem::size_of::<Conn>()) } as *mut Conn;
-	if conn_ptr as *mut core::ffi::c_void == std::ptr::null_mut() {
-		panic!("sshfs: memory allocation failed");
-	};
+    let conn_ptr = unsafe { libc::calloc(1, std::mem::size_of::<Conn>()) } as *mut Conn;
+    if conn_ptr as *mut core::ffi::c_void == std::ptr::null_mut() {
+        panic!("sshfs: memory allocation failed");
+    };
     // Default が実装されていないため、一旦malloc したものをclone する
     let conn = std::sync::Arc::new(unsafe { (*conn_ptr).clone() });
-    unsafe { libc::free(conn_ptr as *mut core::ffi::c_void); };
+    unsafe {
+        libc::free(conn_ptr as *mut core::ffi::c_void);
+    };
     std::sync::Arc::into_raw(conn) as *mut Conn
 }
 
@@ -485,14 +487,30 @@ extern "C" {
         offset: libc::off_t,
         filler: *mut core::ffi::c_void,
     ) -> core::ffi::c_int;
-    fn sshfs_sync_read(sf: *mut SshfsFile, buf: *mut core::ffi::c_char, size: usize,
-                           offset: libc::off_t) -> core::ffi::c_int;
-    fn sshfs_async_read(sf: *mut SshfsFile, buf: *mut core::ffi::c_char, size: usize,
-                           offset: libc::off_t) -> core::ffi::c_int;
-    fn sshfs_sync_write(sf: *mut SshfsFile, buf: *mut core::ffi::c_char, size: usize,
-                           offset: libc::off_t) -> core::ffi::c_int;
-    fn sshfs_async_write(sf: *mut SshfsFile, buf: *mut core::ffi::c_char, size: usize,
-                           offset: libc::off_t) -> core::ffi::c_int;
+    fn sshfs_sync_read(
+        sf: *mut SshfsFile,
+        buf: *mut core::ffi::c_char,
+        size: usize,
+        offset: libc::off_t,
+    ) -> core::ffi::c_int;
+    fn sshfs_async_read(
+        sf: *mut SshfsFile,
+        buf: *mut core::ffi::c_char,
+        size: usize,
+        offset: libc::off_t,
+    ) -> core::ffi::c_int;
+    fn sshfs_sync_write(
+        sf: *mut SshfsFile,
+        buf: *mut core::ffi::c_char,
+        size: usize,
+        offset: libc::off_t,
+    ) -> core::ffi::c_int;
+    fn sshfs_async_write(
+        sf: *mut SshfsFile,
+        buf: *mut core::ffi::c_char,
+        size: usize,
+        offset: libc::off_t,
+    ) -> core::ffi::c_int;
     fn connect_remote(conn: *mut Conn) -> core::ffi::c_int;
     fn sftp_detect_uid(conn: *mut Conn);
     fn process_requests(data: *mut core::ffi::c_void) -> *mut core::ffi::c_void;
@@ -530,43 +548,51 @@ fn get_real_path(path: *const core::ffi::c_char) -> Vec<u8> {
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn start_processing_thread (conn: *mut Conn) -> core::ffi::c_int {
-	if (*conn).processing_thread_started != 0 {
-		0
-	} else if (*conn).rfd == -1 && connect_remote(conn) != 0 {
-		-libc::EIO
-	} else {
-		if retrieve_sshfs().unwrap().detect_uid != 0 {
-			sftp_detect_uid(conn);
-			retrieve_sshfs().unwrap().detect_uid = 0;
-		}
-		// 本来はスタックに持つものだが、未初期化の変数が使用できないためmalloc で確保している
-		let newset = libc::malloc(std::mem::size_of::<libc::sigset_t>()) as *mut libc::sigset_t;
-		let oldset = libc::malloc(std::mem::size_of::<libc::sigset_t>()) as *mut libc::sigset_t;
-		libc::sigemptyset(newset);
-		libc::sigaddset(newset, libc::SIGTERM);
-		libc::sigaddset(newset, libc::SIGINT);
-		libc::sigaddset(newset, libc::SIGHUP);
-		libc::sigaddset(newset, libc::SIGQUIT);
-		libc::pthread_sigmask(libc::SIG_BLOCK, newset, oldset);
-		let conn_org = std::sync::Arc::from_raw(conn);
-		let conn_clone = conn_org.clone();
-		let conn = std::sync::Arc::into_raw(conn_org) as *mut Conn;
-		let builder = std::thread::Builder::new();
-		let handle = builder.spawn(move || { let conn_ptr = std::sync::Arc::into_raw(conn_clone); process_requests(conn_ptr as *mut core::ffi::c_void); std::sync::Arc::from_raw(conn_ptr) });
-		if let Err(err) = handle {
-			eprintln!("failed to create thread: {}", err.kind());
-	    	libc::free(newset as *mut core::ffi::c_void);
-		    libc::free(oldset as *mut core::ffi::c_void);
-			-libc::EIO
-		} else {
-    		libc::pthread_sigmask(libc::SIG_BLOCK, oldset, std::ptr::null_mut() as *mut libc::sigset_t);
-    		(*conn).processing_thread_started = 1;
-	    	libc::free(newset as *mut core::ffi::c_void);
-		    libc::free(oldset as *mut core::ffi::c_void);
-		    0
-		}
-	}
+pub unsafe extern "C" fn start_processing_thread(conn: *mut Conn) -> core::ffi::c_int {
+    if (*conn).processing_thread_started != 0 {
+        0
+    } else if (*conn).rfd == -1 && connect_remote(conn) != 0 {
+        -libc::EIO
+    } else {
+        if retrieve_sshfs().unwrap().detect_uid != 0 {
+            sftp_detect_uid(conn);
+            retrieve_sshfs().unwrap().detect_uid = 0;
+        }
+        // 本来はスタックに持つものだが、未初期化の変数が使用できないためmalloc で確保している
+        let newset = libc::malloc(std::mem::size_of::<libc::sigset_t>()) as *mut libc::sigset_t;
+        let oldset = libc::malloc(std::mem::size_of::<libc::sigset_t>()) as *mut libc::sigset_t;
+        libc::sigemptyset(newset);
+        libc::sigaddset(newset, libc::SIGTERM);
+        libc::sigaddset(newset, libc::SIGINT);
+        libc::sigaddset(newset, libc::SIGHUP);
+        libc::sigaddset(newset, libc::SIGQUIT);
+        libc::pthread_sigmask(libc::SIG_BLOCK, newset, oldset);
+        let conn_org = std::sync::Arc::from_raw(conn);
+        let conn_clone = conn_org.clone();
+        let conn = std::sync::Arc::into_raw(conn_org) as *mut Conn;
+        let builder = std::thread::Builder::new();
+        let handle = builder.spawn(move || {
+            let conn_ptr = std::sync::Arc::into_raw(conn_clone);
+            process_requests(conn_ptr as *mut core::ffi::c_void);
+            std::sync::Arc::from_raw(conn_ptr)
+        });
+        if let Err(err) = handle {
+            eprintln!("failed to create thread: {}", err.kind());
+            libc::free(newset as *mut core::ffi::c_void);
+            libc::free(oldset as *mut core::ffi::c_void);
+            -libc::EIO
+        } else {
+            libc::pthread_sigmask(
+                libc::SIG_BLOCK,
+                oldset,
+                std::ptr::null_mut() as *mut libc::sigset_t,
+            );
+            (*conn).processing_thread_started = 1;
+            libc::free(newset as *mut core::ffi::c_void);
+            libc::free(oldset as *mut core::ffi::c_void);
+            0
+        }
+    }
 }
 
 #[no_mangle]
@@ -1034,14 +1060,14 @@ pub unsafe extern "C" fn sshfs_read(
     offset: libc::off_t,
     fi: &mut fuse_file_info,
 ) -> core::ffi::c_int {
-	let sf = get_sshfs_file(fi);
-	if sshfs_file_is_conn(sf) == 0 {
-		-libc::EIO
-	} else if retrieve_sshfs().unwrap().sync_read != 0 {
-		sshfs_sync_read(sf, rbuf, size, offset)
-	} else {
-		sshfs_async_read(sf, rbuf, size, offset)
-	}
+    let sf = get_sshfs_file(fi);
+    if sshfs_file_is_conn(sf) == 0 {
+        -libc::EIO
+    } else if retrieve_sshfs().unwrap().sync_read != 0 {
+        sshfs_sync_read(sf, rbuf, size, offset)
+    } else {
+        sshfs_async_read(sf, rbuf, size, offset)
+    }
 }
 
 #[no_mangle]
@@ -1052,20 +1078,20 @@ pub unsafe extern "C" fn sshfs_write(
     offset: libc::off_t,
     fi: &mut fuse_file_info,
 ) -> core::ffi::c_int {
-	let sf = get_sshfs_file(fi);
-	if sshfs_file_is_conn(sf) == 0 {
-		-libc::EIO
-	} else {
-		sshfs_inc_modifver();
-		let ret = if retrieve_sshfs().unwrap().sync_write != 0 && (*sf).write_error == 0 {
-		    sshfs_sync_write(sf, wbuf, size, offset)
-	    } else {
-		    sshfs_async_write(sf, wbuf, size, offset)
-		};
-		if ret == 0 {
-			size as core::ffi::c_int
-		} else {
-			ret
-		}
-	}
+    let sf = get_sshfs_file(fi);
+    if sshfs_file_is_conn(sf) == 0 {
+        -libc::EIO
+    } else {
+        sshfs_inc_modifver();
+        let ret = if retrieve_sshfs().unwrap().sync_write != 0 && (*sf).write_error == 0 {
+            sshfs_sync_write(sf, wbuf, size, offset)
+        } else {
+            sshfs_async_write(sf, wbuf, size, offset)
+        };
+        if ret == 0 {
+            size as core::ffi::c_int
+        } else {
+            ret
+        }
+    }
 }
